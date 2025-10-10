af0cc38df81875708596c55193a17b56c7d2d3c0
Bruno da Luz <brunolabx@gmail.com>
2025-10-10 01:17:58 -0300

    Create aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,17 @@
+# ===== GIT =====
+alias gs='git status -sb'
+alias gc='git commit -v'
+alias gco='git checkout'
+alias gpl='git pull --rebase'
+alias gps='git push'
+alias gl='git log --oneline --graph --decorate'
+alias gls='git_list'
+alias ga='git_add'
+alias grs='git_reset'
+
+# ===== SYSTEM =====
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ll='ls -lah'
+alias la='ls -A'
+alias reload='source ~/.zshrc'
